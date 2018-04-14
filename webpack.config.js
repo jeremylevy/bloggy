41e7b97a7b7f9cfb9b3778ad7988f23c16c44291
Jeremy Levy <jje.levy@gmail.com>
2018-04-14 19:17:27 +0200

    Add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,17 @@ Encore
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
+    
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
-
-    // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
-
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    
+    .addEntry('js/app', './assets/js/app.js')
+    .addStyleEntry('css/app', './assets/scss/app.scss')
+    
+    .enableSassLoader()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
